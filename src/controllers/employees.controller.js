36dabbc928462d8eefb0a0519858fb18943c2059
Leonardo Gareca <leoo55guille@gmail.com>
2023-02-03 21:34:27 -0300

    Adaptacion codigo para bd Padron Electoral

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 export const getEmployees = async (req, res) => {
   try {
-    const [rows] = await pool.query("SELECT * FROM padron");
+    const [rows] = await pool.query("SELECT * FROM padron_electoral");
     res.json(rows);
   } catch (error) {
     return res.status(500).json({ message: "Algo fallo, disculpas" });
@@ -11,18 +11,18 @@ export const getEmployees = async (req, res) => {
 
 export const getEmployee = async (req, res) => {
   try {
-    const { id } = req.params;
-    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
-      id,
+    const { dni } = req.params;
+    const [rows] = await pool.query("SELECT * FROM padron_electoral WHERE dni = ?", [
+      dni,
     ]);
 
     if (rows.length <= 0) {
-      return res.status(404).json({ message: "Employee not found" });
+      return res.status(404).json({ message: "Ciudadano no encontrado" });
     }
 
     res.json(rows[0]);
   } catch (error) {
-    return res.status(500).json({ message: "Something goes wrong" });
+    return res.status(500).json({ message: "Algo fallo disculpas" });
   }
 };
 
